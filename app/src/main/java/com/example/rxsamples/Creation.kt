3ccb76b30abe7a4b12d185aea7901115d36b0fc0
Andras Nemeth <guni82hu@gmail.com>
2020-03-02 23:39:45 +0100

    creation

--- app/src/main/java/com/example/rxsamples/Creation.kt
+++ app/src/main/java/com/example/rxsamples/Creation.kt
@@ -1,13 +1,12 @@
 package com.example.rxsamples
 
 import android.annotation.SuppressLint
-import io.reactivex.Emitter
 import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Consumer
-import java.util.concurrent.Callable
+import java.io.IOException
+import java.lang.Exception
+import java.util.concurrent.TimeUnit
 
 @SuppressLint("CheckResult")
 class Creation {
@@ -134,6 +133,76 @@ class Creation {
         observable.subscribe(observer)
         Thread.sleep(500)
         observable.subscribe(observer)
+    }
+
+    fun empty() {
+//        completes instantly
+//        D: onCompleted
+
+        Observable.empty<String>()
+            .subscribe( { log("onNext") }, { log("onError") }, { log("onCompleted") } )
+    }
+
+    fun never() {
+//        it never will do anything
+        Observable.never<String>()
+            .subscribe( { log("onNext") }, { log("onError") }, { log("onCompleted") } )
+    }
+
+    fun error() {
+//        triggers an error
+//        D: exception message
+        Observable.error<Exception>(IOException("exception message"))
+            .subscribe( { log("onNext") }, { log(it.message!!) }, { log("onCompleted") } )
+    }
+
+    fun range() {
+//        D: 3
+//        D: 4
+//        D: 5
+//        D: 6
+//        D: 7
+        Observable.range(3, 5)
+            .subscribe( { log(it.toString()) }, { log("onError") }, { log("onCompleted") } )
+    }
+
+    fun interval() {
+//        It emits an increasing series of Long in every 500 milliseconds and it never completes
+//
+//        D: 0
+//        D: 1
+//        D: 2
+//        D: 3
+//        D: 4
+//        .....
+        Observable.interval(500L, TimeUnit.MILLISECONDS)
+            .subscribe( { log(it.toString()) }, { log("onError") }, { log("onCompleted") } )
+    }
+
+    fun intervalRange() {
+//        Combination of interval and range. After the subscription it waits 2 seconds and then emits the series with 500ms deley bitween the items
+//
+//        D: onSubscribe
+//        D: 100
+//        D: 101
+//        D: 102
+//        D: 103
+//        D: 104
+//        D: onCompleted
+        Observable.intervalRange(100L, 5L, 2000L, 500L, TimeUnit.MILLISECONDS)
+            .doOnSubscribe { log("onSubscribe") }
+            .subscribe( { log(it.toString()) }, { log("onError") }, { log("onCompleted") } )
+    }
+
+    fun timer() {
+//        Emits a 0L value with the defined delay
+//
+//        D: onSubscribe
+//        D: 0
+//        D: onCompleted
+        Observable.timer(3000L, TimeUnit.MILLISECONDS)
+            .doOnSubscribe { log("onSubscribe") }
+            .subscribe( { log(it.toString()) }, { log("onError") }, { log("onCompleted") } )
     }
 
     fun latest() {
