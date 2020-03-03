7775143842a4e8c6bd827c291752b6419d303e03
András Németh <andras.nemeth4@logmein.com>
2020-03-03 13:11:24 +0100

    doOnTerminate Vs DoOnDispose Vs DoFinally

--- app/src/main/java/com/example/rxsamples/MainActivity.kt
+++ app/src/main/java/com/example/rxsamples/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Creation().latest()
         Transformation().latest()
+        RandomThings().latest()
     }
 }
---
+++ app/src/main/java/com/example/rxsamples/RandomThings.kt
@@ -0,0 +1,44 @@
+package com.example.rxsamples
+
+import android.annotation.SuppressLint
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+@SuppressLint("CheckResult")
+class RandomThings {
+
+
+    fun doOnTerminateVsDoOnDisposeVsDoFinally() {
+
+        val observable = Observable.intervalRange(1L, 5L, 0L, 1L, TimeUnit.SECONDS)
+            .doOnTerminate { log("terminated") }
+            .doOnDispose { log("disposed") }
+            .doFinally { log("finally") }
+
+//        D: 1
+//        D: 2
+//        D: 3
+//        D: 4
+//        D: 5
+//        D: terminated
+//        D: completed
+//        D: finally
+        observable
+            .subscribe({ log(it.toString()) }, { log(it.message ?: "empty error message") }, { log("completed") })
+
+//        D: 1
+//        D: 2
+//        D: 3
+//        D: disposed
+//        D: finally
+        val disposable = observable
+            .subscribe( { log(it.toString()) }, { log(it.message ?: "empty error message") }, { log("completed") })
+        TimeUnit.SECONDS.sleep(2)
+        disposable.dispose()
+    }
+
+    fun latest() {
+
+    }
+}
